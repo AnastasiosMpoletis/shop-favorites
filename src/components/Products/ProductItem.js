46e11f081918356b025d17baa95de73aa0c82613
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-07-27 19:57:29 +0300

    L-554. Replace Redux toggleFav with products-context functionality

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
+import { ProductsContext } from '../../context/products-context.js';
 
 const ProductItem = props => {
+  const toggleFav = useContext(ProductsContext).toggleFav;
 
   const toggleFavHandler = () => {
-    
+    toggleFav(props.id);
   };
 
   return (
