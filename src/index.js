7e5aa8087e198f0f45f9d7cce8503a913a92ffe2
AnastasiosMpoletis <anastasiosmpoletis@gmail.com>
2025-07-27 19:44:48 +0300

    L-553. Replace Redux with ProductsContext

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { combineReducers, legacy_createStore } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import productReducer from './store/reducers/products';
-
-const rootReducer = combineReducers({
-  shop: productReducer,
-});
-
-const store = legacy_createStore(rootReducer);
+import ProductsProvider from './context/products-context.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <ProductsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </ProductsProvider>
 );
